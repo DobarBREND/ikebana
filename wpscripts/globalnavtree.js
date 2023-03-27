47fc7cc0772649b0e9d38fa5cb9f12bb12707df5
Vladimir Jovanović <114678399+DobarBREND@users.noreply.github.com>
2023-03-28 00:44:42 +0200

    Add files via upload

---
+++ wpscripts/globalnavtree.js
@@ -0,0 +1,169 @@
+var global_navtree = WpNavBar.readTree({
+"childArray" : [
+{   'bSeparatorAfter':true,
+    'sDescription':'Prodavnica lepih stvari...',
+    'sTitle':'Naslovna',
+    'bIsWebPath':true,
+    'sUrl':'index.html'
+},
+{   'bSeparatorAfter':true,
+    'sDescription':'Umetnost, veština, originalan pristup...',
+    'sTitle':'O nama',
+    'bIsWebPath':true,
+    'sUrl':'onama.html'
+},
+{   'bSeparatorAfter':true,
+    'sDescription':'Obradujte sebe i svoje prijatelje...',
+    'sTitle':'Proizvodi',
+    'bIsWebPath':true,
+    'sUrl':'proizvodi.html',"childArray" : [
+    {   'bSeparatorAfter':true,
+        'sDescription':'Obično neobičan nameštaj...',
+        'sTitle':'Komadni nameštaj',
+        'bIsWebPath':true,
+        'sUrl':'namestaj.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Slike po želji...',
+        'sTitle':'Slike',
+        'bIsWebPath':true,
+        'sUrl':'slike.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Uramite svaku želju...',
+        'sTitle':'Ramovi',
+        'bIsWebPath':true,
+        'sUrl':'ramovi.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Vreme je život...',
+        'sTitle':'Satovi',
+        'bIsWebPath':true,
+        'sUrl':'satovi.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Kuhinjski pribor za uživanje...',
+        'sTitle':'Kuhinjski pribor',
+        'bIsWebPath':true,
+        'sUrl':'kuh_pribor.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Važan deo svakog enterijera...',
+        'sTitle':'Stalci',
+        'bIsWebPath':true,
+        'sUrl':'stalci.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Šolje i šoljice za razne divane...',
+        'sTitle':'Šolje i šoljice',
+        'bIsWebPath':true,
+        'sUrl':'solj_soljice.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Kovčezi i kutije za vaše male tajne...',
+        'sTitle':'Kovčezi i kutije',
+        'bIsWebPath':true,
+        'sUrl':'kov_kutije.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Ljubav na prvi pogled...',
+        'sTitle':'Pliš igračke',
+        'bIsWebPath':true,
+        'sUrl':'plis_igracke.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Sveće i svećnjaci za intimne trenutke...',
+        'sTitle':'Sveće i svećnjaci',
+        'bIsWebPath':true,
+        'sUrl':'sve_svecnjaci.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Korisno i ukrasno...',
+        'sTitle':'Vaze, činije, tacne',
+        'bIsWebPath':true,
+        'sUrl':'vaz_cin_tacne.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Korpe i korpice za razne sitnice...',
+        'sTitle':'Korpe',
+        'bIsWebPath':true,
+        'sUrl':'korpe.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Da osvetlite trenutak...',
+        'sTitle':'Lampe',
+        'bIsWebPath':true,
+        'sUrl':'lampe.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Kao odsjaj u ogledalu...',
+        'sTitle':'Ogledala',
+        'bIsWebPath':true,
+        'sUrl':'ogledala.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Ikone...',
+        'sTitle':'Ikone',
+        'bIsWebPath':true,
+        'sUrl':'ikone.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Idealan poklon za prijatelje...',
+        'sTitle':'Ukrasi i suveniri',
+        'bIsWebPath':true,
+        'sUrl':'ukr_suveniri.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Priroda u vašem domu...',
+        'sTitle':'Saksijsko cveće',
+        'bIsWebPath':true,
+        'sUrl':'sak_cvece.html'
+    }]
+},
+{   'bSeparatorAfter':true,
+    'sDescription':'Znanje, stručnost, iskustvo na usluzi...',
+    'sTitle':'Usluge',
+    'bIsWebPath':true,
+    'sUrl':'usluge.html',"childArray" : [
+    {   'bSeparatorAfter':true,
+        'sDescription':'Ozelenjavanje površina...',
+        'sTitle':'Ozelenjavanje površina',
+        'bIsWebPath':true,
+        'sUrl':'ozelenjavanje.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Aranžmani po želji...',
+        'sTitle':'Aranžmani ',
+        'bIsWebPath':true,
+        'sUrl':'aranzmani.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Biodekoracija za venčanja i svadbe...',
+        'sTitle':'Biodekoracija za venčanja i svadbe',
+        'bIsWebPath':true,
+        'sUrl':'bio_vencanja_svadbe.html'
+    },
+    {   'bSeparatorAfter':true,
+        'sDescription':'Biodekoracija poslovnih prostora...',
+        'sTitle':'Biodekoracija poslovnih prostora',
+        'bIsWebPath':true,
+        'sUrl':'bio_pos_prostori.html'
+    },
+    {   'sTitle':'Aranžmani za poslednji pozdrav i pomen',
+        'bIsWebPath':true,
+        'sUrl':'ara_pos_pozdrav.html'
+    }]
+},
+{   'bSeparatorAfter':true,
+    'sDescription':'Galerija lepote i jedinstvenosti...',
+    'sTitle':'Galerija',
+    'bIsWebPath':true,
+    'sUrl':'galerija.html'
+},
+{   'bSeparatorAfter':true,
+    'sDescription':'Vreme je da se upoznamo...',
+    'sTitle':'Kontakt',
+    'bIsWebPath':true,
+    'sUrl':'kontakt.html'
+}]
+});
